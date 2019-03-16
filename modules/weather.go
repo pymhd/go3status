45ae36dcefe8ca2c6decebb7512bb7ffa326f109
Aram Asatryan <aram808@gmail.com>
2019-03-16 11:27:59 +0300

    detect location once

--- modules/weather.go
+++ modules/weather.go
@@ -1,54 +1,58 @@
 package modules
 
-
 import (
-        "fmt"
-        "time"
-        "strings"
-        "strconv"
-        "net/http"
-        "sync/atomic"
-        "encoding/json"
-)
-const (
-    OpenWeatherMapToken = "af7bfe22287c652d032a3064ffa44088"
-    snowIcon = "\uf2dc"
-    rainIcon = "\uf740" 
-    sunIcon = "\uf185"
-    cloudIcon = "\uf0c2" 
-    thunderIcon = ""
-    windIcon = ""
-    smogIcon = "\uf75f"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync/atomic"
+	"time"
 )
 
-var iconSet map[string]string = map[string]string{"Clear": sunIcon, "Clouds": cloudIcon, "Thunderstorm": thunderIcon, 
-                                                  "Drizzle": rainIcon, "Rain": rainIcon, "Snow": snowIcon}
+const (
+	OpenWeatherMapToken = "af7bfe22287c652d032a3064ffa44088"
+	snowIcon            = "\uf2dc"
+	rainIcon            = "\uf740"
+	sunIcon             = "\uf185"
+	cloudIcon           = "\uf0c2"
+	thunderIcon         = ""
+	windIcon            = "\u2635"
+	smogIcon            = "\uf75f"
+)
+
+var (
+	iconSet map[string]string = map[string]string{"Clear": sunIcon, "Clouds": cloudIcon, "Thunderstorm": thunderIcon,
+		"Drizzle": rainIcon, "Rain": rainIcon, "Snow": snowIcon}
+
+	loc *location
+)
 
 type location struct {
-    Name	string	`json:"city"`
-    Coord	string  `json:"loc"`
-    rewrite	string
+	Name    string `json:"city"`
+	Coord   string `json:"loc"`
+	rewrite string
 }
 
 type weather struct {
-    Weather  []struct {
-            Main  string `json:"main"`
-            Desc  string  `json:"description"`    
-    } `json:"weather"`
-    Main struct {
-        Temp float64 `json:"temp"`
-    } `json:"main"`	
-    Wind  struct {
-        Speed int `json:"speed"`
-    } `json:"wind"`
+	Weather []struct {
+		Main string `json:"main"`
+		Desc string `json:"description"`
+	} `json:"weather"`
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+	Wind struct {
+		Speed int `json:"speed"`
+	} `json:"wind"`
 }
 
 type Weather struct {
-    name string
+	name string
 }
 
 func (w Weather) Name() string {
-    return w.name    
+	return w.name
 }
 
 func (w Weather) Run(c chan ModuleOutput, cfg ModuleConfig) {
@@ -67,41 +71,43 @@ func (w Weather) Run(c chan ModuleOutput, cfg ModuleConfig) {
 	}
 }
 
-func(w Weather) run(c chan ModuleOutput, cfg ModuleConfig) {
-        output := ModuleOutput{}
+func (w Weather) run(c chan ModuleOutput, cfg ModuleConfig) {
+	output := ModuleOutput{}
 	output.Name = w.name
 	output.Instance = strconv.Itoa(cfg.Id)
 	output.Refresh = true
 	output.Markup = "pango"
 	output.Separator = true
 	output.FullText = cfg.Prefix
-	
-	loc := new(location)
-	l, ok := cfg.Extra["location"]
-	if !ok {
-	    loc = getLocation()
-	} else {
-	    city, _ := l.(string)
-	    if city == "auto" {
-	        loc = getLocation()
-	    } else {
-	        //loc = &location{}
-	        loc.rewrite = city 
-	        loc.Name = city
-            }
-	} 
+
+	//if it is first time
+	if loc == nil {
+		l, ok := cfg.Extra["location"]
+		if !ok {
+			loc = getLocation()
+		} else {
+			city, _ := l.(string)
+			if city == "auto" {
+				loc = getLocation()
+			} else {
+				loc = new(location)
+				loc.rewrite = city
+				loc.Name = city
+			}
+		}
+	}
 	wf := getWeather(loc)
 	icon, ok := iconSet[wf.Weather[0].Main]
 	if !ok {
-	    icon = smogIcon
+		icon = smogIcon
 	}
-	forecast := fmt.Sprintf("%s: %s %.0f (%s %d m/s)", loc.Name, icon, wf.Main.Temp, thunderIcon, wf.Wind.Speed)
-	
+	forecast := fmt.Sprintf("%s: %s %.0f (%d m/s)", loc.Name, icon, wf.Main.Temp, wf.Wind.Speed)
+
 	if x := atomic.LoadInt32(Mute[cfg.Id]); x == -1 {
-                output.FullText += forecast[strings.Index(forecast, ":") + 2:]
-        } else {
-                output.FullText += forecast
-        }
+		output.FullText += forecast[strings.Index(forecast, ":")+2:]
+	} else {
+		output.FullText += forecast
+	}
 
 	c <- output
 }
@@ -117,12 +123,12 @@ func (w Weather) HandleClickEvent(ce *ClickEvent, cfg ModuleConfig) {
 		buttonNumber := ce.Button
 		buttonText := clickMap[buttonNumber]
 		cmd, ok := cfg.ClickEvents[buttonText]
-                if !ok {
-                	//if no cmd specified in config file
-                        break
-                }
-                execute(cmd)
-                RefreshChans[cfg.Id] <- true
+		if !ok {
+			//if no cmd specified in config file
+			break
+		}
+		execute(cmd)
+		RefreshChans[cfg.Id] <- true
 
 	}
 }
@@ -131,44 +137,42 @@ func (w Weather) Mute(id int) {
 	atomic.StoreInt32(Mute[id], ^atomic.LoadInt32(Mute[id]))
 }
 
-
 func getLocation() *location {
-        loc := new(location)
-        url := "http://ipinfo.io/json"
-        res, err := http.Get(url)
-        if err != nil {
-            return loc
-        }
-        defer res.Body.Close()
-        if err := json.NewDecoder(res.Body).Decode(loc); err != nil {
-            fmt.Println("Not worked out")
-        }
-        return loc
+	loc := new(location)
+	url := "http://ipinfo.io/json"
+	res, err := http.Get(url)
+	if err != nil {
+		return loc
+	}
+	defer res.Body.Close()
+	if err := json.NewDecoder(res.Body).Decode(loc); err != nil {
+		fmt.Println("Not worked out")
+	}
+	return loc
 }
 
-func getWeather(l *location) *weather{
-        w := new(weather)
-        coord := strings.Split(l.Coord, ",")
-        var url string
-        if len(l.rewrite) > 0 {
-             url = fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?appid=%s&q=%s", OpenWeatherMapToken, l.rewrite)
-        } else {
-            url = fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?appid=%s&lat=%s&lon=%s&units=metric", OpenWeatherMapToken, coord[0], coord[1])            
-        }
-        res, err := http.Get(url)
-        if err != nil {
-            return w
-        }
-        defer res.Body.Close()
-        if err := json.NewDecoder(res.Body).Decode(w); err != nil {
-            fmt.Println("Not worked out")
-        }
-        return w
-
+func getWeather(l *location) *weather {
+	w := new(weather)
+	coord := strings.Split(l.Coord, ",")
+	var url string
+	if len(l.rewrite) > 0 {
+		url = fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?appid=%s&q=%s", OpenWeatherMapToken, l.rewrite)
+	} else {
+		url = fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?appid=%s&lat=%s&lon=%s&units=metric", OpenWeatherMapToken, coord[0], coord[1])
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		return w
+	}
+	defer res.Body.Close()
+	if err := json.NewDecoder(res.Body).Decode(w); err != nil {
+		fmt.Println("Not worked out")
+	}
+	return w
 
 }
 
 func init() {
-    w := Weather{name: "weather"}
-    selfRegister(w)
+	w := Weather{name: "weather"}
+	selfRegister(w)
 }
