a8db04514a515c735c55168ab96102c76448ef9a
George Kakovkin <georgii_kakovkin@epam.com>
2019-03-22 18:23:33 +0300

    cache support

---
+++ modules/cache.go
@@ -0,0 +1,8 @@
+package modules
+
+import (
+        cachelib "github.com/pymhd/go-simple-cache"
+)
+
+var cache = cachelib.New("/tmp/go3status-cache.json")
+
--- modules/cpu.go
+++ modules/cpu.go
@@ -1,7 +1,18 @@
 package modules
 
+import (
+        "strconv"
+)
+
 func cpu(mo *ModuleOutput, cfg ModuleConfig) {
-        mo.FullText += "70%"
+        var percentage int
+        v := cache.Get("cpu:percentage")
+        if v != nil {
+                percentage, _ = v.(int)
+        }
+        mo.FullText += strconv.Itoa(percentage)
+        percentage++
+        cache.Add("cpu:percentage", percentage, "1h")
 }
 
 func init() {
