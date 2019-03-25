0734ad52b0a7da3b01b05bed087553c0dc76c429
Ilya Martinov <ilyanix@gmail.com>
2019-03-26 00:19:38 +0300

    modules/batt.go new funtion for batt

--- modules/batt.go
+++ modules/batt.go
@@ -9,51 +9,19 @@ import (
 	"strconv"
 )
 
-type BAT struct {
-	name string
-}
-
-func (batt BAT) Name() string {
-        return batt.name
-}
-
-func (batt BAT) Run(c chan ModuleOutput, cfg ModuleConfig) {
-        //to run by start
-        batt.run(c, cfg)
-
-        // to run periodically
-        ticker := time.NewTicker(cfg.Interval)
-        for {
-                select {
-                case <-ticker.C:
-                        batt.run(c, cfg)
-                case <-RefreshChans[cfg.Id]:
-                        batt.run(c, cfg)
-                }
-        }
-}
-
-func (batt BAT) run(c chan ModuleOutput, cfg ModuleConfig) {
+func batt(mo *ModuleOutput, cfg ModuleConfig) {
         output := ModuleOutput{}
-	output.Name = batt.name
         output.Markup = "pango"
         output.Refresh = true
         output.FullText = cfg.Prefix
 
 	percentage, capacity, status := getBatPercent()
-        for lvl, val := range cfg.Levels {
-		if inRange(percentage, val) {
-			output.Color = cfg.Colors[lvl]
-                }
-	}
         duration, _ := time.ParseDuration(capacity)
         hours, minutes := fmtDuration(duration)
-	output.FullText += fmt.Sprintf("%.0f%% %s(%dh%dm)", percentage, status, hours, minutes)
-        c <- output
+	mo.Color = getColor(percentage, cfg)
+	mo.FullText += fmt.Sprintf("%.0f%% %s(%dh%dm)", percentage, status, hours, minutes)
 }
 
-func (batt BAT) HandleClickEvent(ce *ClickEvent, cfg ModuleConfig) {
-}
 
 func fmtDuration(d time.Duration) (time.Duration, time.Duration) {
     d = d.Round(time.Minute)
@@ -66,8 +34,7 @@ func fmtDuration(d time.Duration) (time.Duration, time.Duration) {
 
 func getBatPercent () (float64, string, string) {
         var b_status, capacity string
-	var e_full, e_now, p_now int
-	var percent float64
+	var e_full, e_now, p_now, percent float64
 
 	re := regexp.MustCompile(`(^.*)=(.*$)`)
 	file, _ := os.Open("/sys/class/power_supply/BAT0/uevent")
@@ -81,23 +48,23 @@ func getBatPercent () (float64, string, string) {
 			case "POWER_SUPPLY_STATUS":
 				b_status = match[2]
 			case "POWER_SUPPLY_POWER_NOW":
-				p_now, _ = strconv.Atoi(match[2])
+				p_now, _ = strconv.ParseFloat(match[2], 64)
 			case "POWER_SUPPLY_ENERGY_NOW":
-				e_now, _ = strconv.Atoi(match[2])
+				e_now, _ = strconv.ParseFloat(match[2], 64)
 			case "POWER_SUPPLY_ENERGY_FULL":
-				e_full, _ = strconv.Atoi(match[2])
+				e_full, _ = strconv.ParseFloat(match[2], 64)
 			}
 		}
 	}
 
-	percent = float64(100 * e_now / e_full)
+	percent = 100 * e_now / e_full
 	switch b_status {
 	case "Discharging":
-		c := float64(e_now) / float64(p_now)
+		c := e_now / p_now
 		capacity = fmt.Sprintf("%.7fh", c)
 		b_status = "DIS"
 	case "Charging":
-		c := (float64(e_full) - float64(e_now)) / float64(p_now)
+		c := (e_full - e_now) / p_now
 		capacity = fmt.Sprintf("%.7fh", c)
 		b_status = "CHR"
 	}
@@ -106,6 +73,5 @@ func getBatPercent () (float64, string, string) {
 }
 
 func init() {
-	batt := BAT{name: "batt"}
-	selfRegister(batt)
+	RegisteredFuncs["batt"] = batt
 }
