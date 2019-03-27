adbad330dff6737cffcc7e67728a40dca39fbb0b
George Kakovkin <georgii_kakovkin@epam.com>
2019-03-27 18:11:38 +0300

    Some logs added

--- events.go
+++ events.go
@@ -14,19 +14,22 @@ func RunClickEventsHandler(sl *StatusLine) {
 	for scanner.Scan() {
 		b := scanner.Bytes()
 		ce := new(modules.ClickEvent)
-
 		if json.Valid(b) {
+			log.Info("New valid Click Event received")
 			json.Unmarshal(b, ce)
 		} else {
 			if err := json.Unmarshal(b[1:], ce); err != nil {
 				// skip, cant do nothing with
+				log.Errorf("Could not unmarshal JSON obj (%s)\n", err)
 				continue
 			}
+			log.Warning("Parsed json with extra char")
 		}
 		//not strange stdin
 		if len(ce.Name) > 0 {
 			id, _ := strconv.Atoi(ce.Instance)
 			go sl.Modules[id].HandleClickEvent(ce)
+			log.Infof("Ran %q module click event handler routine\n", sl.Modules[id].Name)
 		}
 	}
 }
