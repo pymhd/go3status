2de776f6695e224270d61ac986e2e4828fd9e765
George Kakovkin <georgii_kakovkin@epam.com>
2019-05-14 14:39:26 +0300

    additional opt in config to enable feature

--- internal/modules/exec.go
+++ internal/modules/exec.go
@@ -30,44 +30,52 @@ func execCmd(mo *ModuleOutput, cfg ModuleConfig) {
 			}
 		}
 	}
-
-	update := time.Duration(10 * time.Second)
-	upd, ok := cfg.Extra["update"]
+	var cacheEnabled bool
+	_, ok = cfg.Extra["cache"]
 	if ok {
-		ts, ok := upd.(string)
+		cacheEnabled, _ = cfg.Extra["cache"].(bool)	
+	}
+	if cacheEnabled {
+		update := time.Duration(10 * time.Second)
+		upd, ok := cfg.Extra["update"]
 		if ok {
-			t, err := time.ParseDuration(ts)
-			if err == nil {
-				update = t
+			ts, ok := upd.(string)
+			if ok {
+				t, err := time.ParseDuration(ts)
+				if err == nil {
+					update = t
+				}
 			}
 		}
-	}
-	//if it is first module run
-	if workerSpawned := cache.Get(cmd + spawned); workerSpawned == nil {
-		updateTicker := time.NewTicker(update)
-		go func() {
-			cache.Add(cmd+spawned, true, "365d")
-			//exec cmd right now then periodically
-			o := execute(cmd, timeout)
-			cache.Add(cmd, o, "1h")
-
-			for range updateTicker.C {
+		//if it is first module run
+		if workerSpawned := cache.Get(cmd + spawned); workerSpawned == nil {
+			updateTicker := time.NewTicker(update)
+			go func() {
+				cache.Add(cmd+spawned, true, "365d")
+				//exec cmd right now then periodically
 				o := execute(cmd, timeout)
-				cache.Add(cmd, o, "24h")
-			}
-		}()
+				cache.Add(cmd, o, "1h")
 
-	}
-	// if worker already was spawned then we wiil wait for latest cache value
-	for {
-		output, _ := cache.Get(cmd).(string)
-		if len(output) > 0 {
-			mo.FullText += output
-			return
+				for range updateTicker.C {
+					o := execute(cmd, timeout)
+					cache.Add(cmd, o, "24h")
+				}
+			}()
+
 		}
-		time.Sleep(100 * time.Millisecond)
+		// if worker already was spawned then we wiil wait for latest cache value
+		for {
+			output, _ := cache.Get(cmd).(string)
+			if len(output) > 0 {
+				mo.FullText += output
+				return
+			}
+			time.Sleep(100 * time.Millisecond)
+		}
 	}
-
+	//if cache is not used fir this module
+	mo.FullText += execute(cmd, timeout)
+	
 }
 
 func init() {
